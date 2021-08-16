8e552584fd9a4676c77730deab81606c8c8315be
haggyg <harry@geyer.co.uk>
2021-08-16 21:26:10 +0100

    Generate the gnuplot scripts in the C program

--- markovian/run.sh
+++ markovian/run.sh
@@ -1,4 +1,21 @@
 #! /bin/sh
 
-gcc -lgmp main.c -Werror -pedantic -o main
-./main
+ABSDIR="$(dirname $(readlink -f $0))"
+
+date
+
+CFLAGS="-pedantic -Wall -Werror -ggdb3"
+CLIBS="-lm -lgmp"
+SRCDIR="${ABSDIR}/src"
+OUTDIR="${ABSDIR}/output"
+
+gcc ${CLIBS} ${SRCDIR}/main.c ${CFLAGS} -o ${OUTDIR}/main
+
+if [[ $? -ne 0 ]]; then
+    echo "Failed to compile"
+    exit 1
+fi
+
+echo "Compiled successfully"
+./output/main
+
