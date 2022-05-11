39f6b3aeadd506efb374c076970777e6af8ac7f8
haggyg <haggyg@gmail.com>
2022-05-11 22:18:39 +0100

    Added a combobox to chose from the different simulation types.

--- markovian/src/gui.c
+++ markovian/src/gui.c
@@ -12,6 +12,45 @@
 #include "data.h"
 
 
+typedef enum
+{
+    SIMULATION_MARKOVIAN_SIR,
+    SIMULATION_MARKOVIAN_SIS,
+} simulation_enum_t;
+
+
+#define SIMULATIONS                                                    \
+{                                                                      \
+    { SIMULATION_MARKOVIAN_SIR, "Markovian SIR", modelling_simulate }, \
+    { SIMULATION_MARKOVIAN_SIS, "Markovian SIS", modelling_simulate }, \
+}
+
+#define SIMULATIONS_COUNT                                           2
+#define MAX_SIM_NAME_LEN                                            16
+
+
+typedef struct
+{
+    simulation_enum_t   id;
+    char                name[MAX_SIM_NAME_LEN];
+    void                (*cb)(context_t* context);
+} simulation_struct_t;
+
+
+static simulation_struct_t  simulations[]       = SIMULATIONS;
+static simulation_enum_t    active_sim_index    = 0;
+
+
+static gboolean _gui_simulation_selection_cb(GtkComboBox* combo_box, context_t* context)
+{
+    int sim_index = gtk_combo_box_get_active(combo_box);
+    if (sim_index >= SIMULATIONS_COUNT)
+        return FALSE;
+    active_sim_index = sim_index;
+    return TRUE;
+}
+
+
 static gboolean _gui_initial_susceptibles_cb(GtkSpinButton *spin_button, context_t* context)
 {
     context->initial_susceptibles = gtk_spin_button_get_value_as_int(spin_button);
@@ -61,12 +100,12 @@ static gboolean _gui_num_iterations_cb(GtkSpinButton *spin_button, context_t* co
 }
 
 
-static gboolean _gui_simulate_cb(GtkSpinButton *spin_button, context_t* context)
+static gboolean _gui_simulate_cb(GtkButton *button, context_t* context)
 {
     clock_t begin = clock();
     srand(time(NULL));
 
-    modelling_simulate(context);
+    simulations[active_sim_index].cb(context);
 
     graph_set_points(context->bins);
 
@@ -83,6 +122,22 @@ static gboolean _gui_simulate_cb(GtkSpinButton *spin_button, context_t* context)
 
     gtk_image_new_from_file(DATA_DIR"/graph.png");
     return TRUE;
+}
+
+
+static void _gui_populate_sim_combo_box(GObject* combo_box)
+{
+    GtkTreeIter iter;
+    GtkListStore *store = gtk_list_store_new (1, G_TYPE_STRING);
+    for (uint8_t i = 0; i < SIMULATIONS_COUNT; i++)
+    {
+        gtk_list_store_append(store, &iter);
+        gtk_list_store_set(store, &iter, 0, simulations[i].name, -1);
+    }
+    gtk_combo_box_set_model(GTK_COMBO_BOX(combo_box), GTK_TREE_MODEL(store));
+    GtkCellRenderer* cell = gtk_cell_renderer_text_new();
+    gtk_cell_layout_set_attributes(GTK_CELL_LAYOUT(combo_box), cell, "text", 0, NULL );
+    gtk_combo_box_set_active(GTK_COMBO_BOX(combo_box),0);
 }
 
 
@@ -104,6 +159,10 @@ void gui_init(context_t* context, int* argc, char*** argv)
     window  = GTK_WIDGET(gtk_builder_get_object(builder,"window1"));
 
     gtk_builder_connect_signals(builder,NULL);
+
+    GObject* simulation_selection_box = gtk_builder_get_object(builder, "simulation_selection_box");
+    _gui_populate_sim_combo_box(simulation_selection_box);
+    g_signal_connect(simulation_selection_box, "changed", G_CALLBACK(_gui_simulation_selection_cb), context);
 
     GObject* initial_susceptibles_spin_btn = gtk_builder_get_object(builder, "initial_susceptibles_spin_btn");
     g_signal_connect(initial_susceptibles_spin_btn, "changed", G_CALLBACK(_gui_initial_susceptibles_cb), context);
